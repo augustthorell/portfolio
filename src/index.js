15071f2e941cd671c430cb4a8b4b702ff4563308
August Thorell <augustthorell@pop-os.localdomain>
2021-07-20 12:48:29 +0200

    Changed Hashrouter to BrowserRouter

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { HashRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import './style/index.css';
 import App from './App';
 
